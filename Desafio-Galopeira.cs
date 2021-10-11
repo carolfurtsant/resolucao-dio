dbcb552c7464946ab5264b77e6708f09c83bd1d6
Ana Caroline <carolfurtadosantos@outlook.com>
2021-10-11 19:03:15 -0300

    Adicionando arquivo existente

---
+++ Desafio-Galopeira.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+classe Teste
+{
+
+    static void Main(string[] args)
+{
+
+    int x = int.Parse(Console.ReadLine());
+    para(int i = 0; i < x; i++)
+      {
+    string C = Console.ReadLine();
+    duplo T = C.Length * 0,01;
+
+    Console.WriteLine("{0: F}", T);
+}
+    }
+}
