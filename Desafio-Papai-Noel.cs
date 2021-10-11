9306f91d77c13eea6ebf36fd9dfe1ae78e2ca015
Ana Caroline <carolfurtadosantos@outlook.com>
2021-10-11 20:09:59 -0300

    Adicionando arquivo

---
+++ Desafio-Papai-Noel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _52._36_URI_1759
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 0; i < N; i++)
+            {
+                if (i < N - 1)
+                {
+                    Console.Write("Ho ");                  //Complete o código no espaço em branco
+                }
+                else
+                {
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
