50bf5792fd70c7f58ff93c2dfc1fbc2cce2eda35
Ana Caroline <carolfurtadosantos@outlook.com>
2021-10-11 21:24:00 -0300

    Adicionando arquivo

---
+++ Pneu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class TESTE
+{
+    static void Main()
+    {
+        var A = int.Parse(Console.ReadLine());
+        var B = int.Parse(Console.ReadLine());
+
+        //Termine o código 
+
+        var result = A - B;
+
+        Console.WriteLine(result);
+
+    }
+
+}
